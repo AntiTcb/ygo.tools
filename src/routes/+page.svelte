9866680788910acefa5fb02635bdce8baf15afbe
Alex Gravely <tcbskater@hotmail.com>
2024-06-25 22:43:59 -0400

    changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,34 @@
+<script lang="ts">
+  import Monster from './Monster.svelte';
+</script>
+
 <div class="flex flex-col gap-4 md:grid md:grid-cols-2">
   <div id="attacking" class="card">
-    <header>Attacking Monster</header>
-    <article></article>
+    <header>Player A: Attacking Monster</header>
+    <Monster />
   </div>
   <div id="defending" class="card">
-    <header>Defending Monster</header>
-    <article></article>
+    <header>Player B: Defending Monster</header>
+    <Monster />
   </div>
-  <div id="result" class="card col-span-2">Results</div>
+  <div id="modifications" class="card col-span-2">
+    <header>Modifications</header>
+    <article>
+      <ul>
+        <li>Player A's Modifiers:</li>
+        <li>Player B's Modifiers:</li>
+      </ul>
+    </article>
+  </div>
+  <div id="result" class="card col-span-2">
+    <header>Battle Result</header>
+    <article>
+      <ul>
+        <li>Damage Player A Takes:</li>
+        <li>Damage Player B Takes:</li>
+      </ul>
+    </article>
+  </div>
 </div>
 
 <style lang="postcss">
---
+++ src/routes/Monster.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  type MonsterProps = {
+    atk: number;
+    def: number | null;
+    position: 'ATK' | 'DEF';
+  };
+  let { atk = $bindable(0), def = $bindable(), position = $bindable('ATK') }: MonsterProps = $props();
+
+  $inspect({ atk, def, position });
+</script>
+
+<article>
+  <div class="inline-flex gap-4">
+    <label class="label" for="atk">
+      <span class="label-text">ATK</span>
+      <input class="input" type="number" id="atk" bind:value={atk} />
+    </label>
+
+    <label class="label" for="atk">
+      <span class="label-text">DEF</span>
+      <input class="input" type="number" id="atk" bind:value={def} />
+    </label>
+  </div>
+
+  <label class="label" for="position">
+    <span class="label-text">Position</span>
+    <div class="flex gap-4">
+      <label class="flex items-center space-x-2">
+        <p>ATK</p>
+        <input type="radio" name="pos" id="pos" class="radio" value="atk" bind:group={position} />
+      </label>
+      <label class="flex items-center space-x-2">
+        <p>DEF</p>
+        <input type="radio" name="pos" id="pos" class="radio" value="def" bind:group={position} />
+      </label>
+    </div>
+  </label>
+</article>
