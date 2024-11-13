9fd9be19d7bd252db8eca553a8761f94ec565a45
Alex Gravely <tcbskater@hotmail.com>
2024-11-13 12:02:22 -0500

    Fix skeleton components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
   import Header from '$components/Header.svelte';
   import Sidebar from '$components/Sidebar.svelte';
   import { setArtworksState } from '$lib/assets/yugiohArtwork.svelte';
-  import { Nav } from '@skeletonlabs/skeleton-svelte';
+  import { Navigation } from '@skeletonlabs/skeleton-svelte';
   import { Toaster } from 'svelte-sonner';
   import CalculatorIcon from 'virtual:icons/mdi/calculator';
   import DatabaseIcon from '~icons/mdi/database-search';
@@ -40,16 +40,16 @@
 <Toaster />
 
 {#snippet NavTiles()}
-  <Nav.Tile href="/damagecalc" id="damagecalc">
+  <Navigation.Tile href="/damagecalc" id="damagecalc">
     <div class="flex flex-col items-center text-center">
       <CalculatorIcon class="size-[24px] md:size-[32px]" />
       <span class="text-sm">Damage Calculator</span>
     </div>
-  </Nav.Tile>
-  <Nav.Tile href="/database" id="database">
+  </Navigation.Tile>
+  <Navigation.Tile href="/database" id="database" classes="h-full">
     <div class="flex flex-col items-center text-center">
       <DatabaseIcon class="size-[24px] md:size-[32px]" />
       <span class="text-sm">Database</span>
     </div>
-  </Nav.Tile>
+  </Navigation.Tile>
 {/snippet}
