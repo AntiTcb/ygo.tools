a46bd98b784cad3f67a60ea86daafb5aeaf2dbfc
Alex Gravely <tcbskater@hotmail.com>
2025-05-07 23:28:31 -0400

    Improves damage calculator and updates version
    Updates the displayed version number in the header.
    
    Enhances the damage calculator by introducing a responsive table layout that adapts to different screen sizes.
    Adds a "Net Difference" column to provide a clearer summary of the battle results.

--- src/routes/damagecalc/+page.svelte
+++ src/routes/damagecalc/+page.svelte
@@ -47,42 +47,106 @@
 <div class="flex flex-col gap-4 lg:grid lg:grid-cols-2">
   <div id="result" class="card col-span-2">
     <header class="card-header">Battle Result:</header>
-    <table class="table-hover table w-auto">
-      <thead>
-        <tr>
-          <th class="min-w-32"></th>
-          <th class="text-right! font-bold!">Player A</th>
-          <th class="text-right! font-bold!">Player B</th>
-        </tr>
-      </thead>
-      <tbody class="hover:[&>tr]:preset-tonal-primary text-right [&>tr>td:first-child]:italic">
-        <tr>
-          <td>Battle Damage</td>
-          <td>{calc.battleResult.playerA.battleDamage}</td>
-          <td>{calc.battleResult.playerB.battleDamage}</td>
-        </tr>
-        <tr>
-          <td>Effect Damage</td>
-          <td>{calc.battleResult.playerA.effectDamage}</td>
-          <td>{calc.battleResult.playerB.effectDamage}</td>
-        </tr>
-        <tr>
-          <td>Redirected Battle Damage</td>
-          <td>{calc.battleResult.playerA.redirectedDamage}</td>
-          <td>{calc.battleResult.playerB.redirectedDamage}</td>
-        </tr>
-        <tr>
-          <td>Redirected Effect Damage</td>
-          <td>{calc.battleResult.playerA.redirectedEffectDamage}</td>
-          <td>{calc.battleResult.playerB.redirectedEffectDamage}</td>
-        </tr>
-        <tr>
-          <td>Life Gained</td>
-          <td>{calc.battleResult.playerA.lifeGained}</td>
-          <td>{calc.battleResult.playerB.lifeGained}</td>
-        </tr>
-      </tbody>
-    </table>
+    <!-- Desktop Table -->
+    <div class="table-wrap hidden lg:table">
+      <table class="table w-auto">
+        <thead>
+          <tr>
+            <th class="min-w-32"></th>
+            <th class="text-right! font-bold!">Battle Damage</th>
+            <th class="text-right! font-bold!">Effect Damage</th>
+            <th class="text-right! font-bold!">Redirected Battle Damage</th>
+            <th class="text-right! font-bold!">Redirected Effect Damage</th>
+            <th class="text-right! font-bold!">Life Gained</th>
+            <th class="text-right! font-bold!">Net Difference</th>
+          </tr>
+        </thead>
+        <tbody class="[&>tr]:hover:preset-tonal-primary text-right [&>tr>td:first-child]:italic">
+          <tr>
+            <td>Player A</td>
+            <td>{calc.battleResult.playerA.battleDamage}</td>
+            <td>{calc.battleResult.playerA.effectDamage}</td>
+            <td>{calc.battleResult.playerA.redirectedDamage}</td>
+            <td>{calc.battleResult.playerA.redirectedEffectDamage}</td>
+            <td>{calc.battleResult.playerA.lifeGained}</td>
+            <td
+              >{calc.battleResult.playerA.battleDamage +
+                calc.battleResult.playerA.effectDamage -
+                calc.battleResult.playerA.redirectedDamage -
+                calc.battleResult.playerA.redirectedEffectDamage +
+                calc.battleResult.playerA.lifeGained}</td>
+          </tr>
+          <tr>
+            <td>Player B</td>
+            <td>{calc.battleResult.playerB.battleDamage}</td>
+            <td>{calc.battleResult.playerB.effectDamage}</td>
+            <td>{calc.battleResult.playerB.redirectedDamage}</td>
+            <td>{calc.battleResult.playerB.redirectedEffectDamage}</td>
+            <td>{calc.battleResult.playerB.lifeGained}</td>
+            <td
+              >{calc.battleResult.playerB.battleDamage +
+                calc.battleResult.playerB.effectDamage -
+                calc.battleResult.playerB.redirectedDamage -
+                calc.battleResult.playerB.redirectedEffectDamage +
+                calc.battleResult.playerB.lifeGained}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- Mobile Table -->
+    <div class="table-wrap block lg:hidden">
+      <table class="table w-auto">
+        <thead>
+          <tr>
+            <th class="min-w-32"></th>
+            <th class="text-right! font-bold!">Player A</th>
+            <th class="text-right! font-bold!">Player B</th>
+          </tr>
+        </thead>
+        <tbody class="[&>tr]:hover:preset-tonal-primary text-right [&>tr>td:first-child]:italic">
+          <tr>
+            <td>Battle Damage</td>
+            <td>{calc.battleResult.playerA.battleDamage}</td>
+            <td>{calc.battleResult.playerB.battleDamage}</td>
+          </tr>
+          <tr>
+            <td>Effect Damage</td>
+            <td>{calc.battleResult.playerA.effectDamage}</td>
+            <td>{calc.battleResult.playerB.effectDamage}</td>
+          </tr>
+          <tr>
+            <td>Redirected Battle Damage</td>
+            <td>{calc.battleResult.playerA.redirectedDamage}</td>
+            <td>{calc.battleResult.playerB.redirectedDamage}</td>
+          </tr>
+          <tr>
+            <td>Redirected Effect Damage</td>
+            <td>{calc.battleResult.playerA.redirectedEffectDamage}</td>
+            <td>{calc.battleResult.playerB.redirectedEffectDamage}</td>
+          </tr>
+          <tr>
+            <td>Life Gained</td>
+            <td>{calc.battleResult.playerA.lifeGained}</td>
+            <td>{calc.battleResult.playerB.lifeGained}</td>
+          </tr>
+          <tr>
+            <td>Net Difference</td>
+            <td
+              >{calc.battleResult.playerA.battleDamage +
+                calc.battleResult.playerA.effectDamage -
+                calc.battleResult.playerA.redirectedDamage -
+                calc.battleResult.playerA.redirectedEffectDamage +
+                calc.battleResult.playerA.lifeGained}</td>
+            <td
+              >{calc.battleResult.playerB.battleDamage +
+                calc.battleResult.playerB.effectDamage -
+                calc.battleResult.playerB.redirectedDamage -
+                calc.battleResult.playerB.redirectedEffectDamage +
+                calc.battleResult.playerB.lifeGained}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 
   <div class="card col-span-2">
@@ -100,7 +164,7 @@
     <Monster bind:monster={calc.attackingMonster} />
 
     <details bind:open>
-      <summary class="text-xl font-bold">Modifiers</summary>
+      <summary class="text-lg font-bold">Modifiers</summary>
       <BattleModifiers bind:modifiers={calc.playerAModifiers} bind:showExamples />
     </details>
   </div>
@@ -109,7 +173,7 @@
     <Monster bind:monster={calc.defendingMonster} defending />
 
     <details bind:open>
-      <summary class="text-xl font-bold">Modifiers</summary>
+      <summary class="text-lg font-bold">Modifiers</summary>
       <BattleModifiers bind:modifiers={calc.playerBModifiers} bind:showExamples />
     </details>
   </div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,11 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const pkg = JSON.parse(json);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,6 +17,9 @@ const config = {
       $components: './src/lib/components',
       $runes: './src/lib/runes',
       $static: './static',
+    },
+    version: {
+      name: pkg.version,
     },
   },
 };
