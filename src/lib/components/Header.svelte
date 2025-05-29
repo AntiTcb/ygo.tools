4c3703acf08c7fb600f8e8d6b57835f700320e81
Alex Gravely <tcbskater@hotmail.com>
2025-05-29 01:04:56 -0400

    Add umami tracking

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,6 +4,10 @@
   import GitHubIcon from '~icons/mdi/github-box';
 </script>
 
+<svelte:head>
+  <script defer src="https://analytics.alexgravely.dev/script.js" data-website-id="0f531f3e-65fb-462c-afd8-83b69e859c89"></script>
+</svelte:head>
+
 <header class="bg-surface-950 flex h-12 items-center justify-between px-2">
   <a href="/" class="flex items-center gap-4 font-bold">
     <img src={SiteLogo} alt="Site Logo" class="max-h-[32px] max-w-[32px]" />
