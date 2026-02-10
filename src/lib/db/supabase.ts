da06cda2307257a09866b55785b30f4438412561
Alex Gravely <tcbskater@hotmail.com>
2026-02-10 16:32:55 -0500

    Remove unused SB file

--- src/lib/db/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { env } from '$env/dynamic/public';
-import { createClient } from '@supabase/supabase-js';
-import type { Database } from './database.types';
-
-export const supabase = createClient<Database>(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
-
-export const searchCards = async (cardName: string) => await supabase.rpc('search_cards', { card_name: cardName });
